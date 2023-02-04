fbcd52c3478476ce60a1b835592abadd88b50389
PratibhaChavan <92856684+PratibhaChavan@users.noreply.github.com>
2023-02-04 00:04:57 -0800

    Update Select All.sql

--- sql/Basic Select/Select All.sql
+++ sql/Basic Select/Select All.sql
@@ -1,7 +1,16 @@
-###Select Al
-
+###Select All
 Query all columns for every row in the CITY table.
-
-Input Format
-
 The CITY table is described as follows:
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+--Solution
+
+SELECT * FROM CITY;
